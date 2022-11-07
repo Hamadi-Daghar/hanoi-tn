57508eb9463ca87008ea3e84ee660ee8cd548fd6
KushiieDev <riviere.lisa1875@gmail.com>
2022-11-07 11:20:11 +0100

    Test

--- hanoi/app.py
+++ hanoi/app.py
@@ -9,7 +9,7 @@ class App(ctk.CTk):
 
     self.title("Hanoi mockup")
     self.geometry("1920x1080")
-    self.resizable(False, False)
+    #self.resizable(False, False)
     self.attributes("-fullscreen", True)
 
     ctk.set_appearance_mode("system")
